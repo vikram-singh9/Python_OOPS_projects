3b35de9338916591e76494d9be71b44e2af64a8b
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-06-03 23:15:40 +0500

    Update main.py

--- car-rental-system/main.py
+++ car-rental-system/main.py
@@ -8,3 +8,27 @@ class Car:
     def show_info(self):
         status = 'rented' if self.is_rented else 'available'
         print(f'{self.brand} {self.model} {self.year} - {status}')
+
+
+class carRentalSystem:
+    def __init__(self):
+        self.cars = []
+
+    def add_car(self, car):
+        self.cars.append(car)
+        print(f'{self.car} added successfuly!')
+
+    def view_available_cars(self):
+        print('\n🚗 Available Cars')
+        available = False
+        for car in self.cars:
+            if not car.is_rented:
+                car.show_info()
+                available = True
+        if not available:
+            print('cars are not available!!')
+
+
+
+
+
