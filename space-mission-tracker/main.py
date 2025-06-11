51ba7d430dac905560ebb194034df41cac6afd0e
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-06-12 01:19:38 +0500

    Update main.py

--- space-mission-tracker/main.py
+++ space-mission-tracker/main.py
@@ -17,7 +17,44 @@ class MissionTracker:
         self.missions.append(mission)
         print(f'mission {mission.name} has been added.')
 
-    def view_mission(self):
+    def view_all_mission(self):
+        print('=== All Missions ===')
+        if not self.missions:
+            print('No missions available.')
+        else:
+            for mission in self.missions:
+                mission.show_info()
+
+    def filter_by_status(self, status):
+                
+        print(f"\n🔍 Missions with status '{status}':")
+        found = False
+
+        for mission in self.missions:
+            if mission.status.lower() == status.lower():
+                mission.show_info()
+                found = True
+
+        if not found:
+            print(f'No missions found with status: {status}')
+
+
+mission1 = Mission('Mars Rover', 'NASA', 2020, 'Completed')
+mission2 = Mission('Lunar Gateway', 'ESA', 2021, 'In Progress')
+mission3 = Mission('James Webb Telescope', 'NASA', 2022, 'Completed')
+
+
+mission_tracker = MissionTracker()
+mission_tracker.add_mission(mission1)
+mission_tracker.add_mission(mission2)
+mission_tracker.add_mission(mission3)
+
+
+mission_tracker.filter_by_status('Completed')
+
+mission_tracker.view_all_mission()
         
+
+
         
         
