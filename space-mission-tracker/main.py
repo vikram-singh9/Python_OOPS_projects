0c571925d86fcd3a517b672a2042c7c88d2e1dc9
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-06-12 01:05:56 +0500

    Update main.py

--- space-mission-tracker/main.py
+++ space-mission-tracker/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from space-mission-tracker!")
+class Mission:
+    def __init__(self, name, agency, year, status):
+        self.name = name
+        self.agency = agency
+        self.year = year
+        self.status = status
+    def show_info(self):
+        print(f'Mission Name: {self.name}, Agency: {self.agency}, Year: {self.year}, Status: {self.status}')
 
 
-if __name__ == "__main__":
-    main()
+class MissionTracker:
+    def __init__(self):
+        self.missions = []
+
+
+    def add_mission(self, mission):
+        self.missions.append(mission)
+        print(f'mission {mission.name} has been added.')
+
+    def view_mission(self):
+        
+        
+        
